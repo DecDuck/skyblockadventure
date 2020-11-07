90568cc035db084640e7ed2c8b215ca208608c3c
decduck3 <decoftheduck@gmail.com>
2020-11-07 16:18:47 +1100

    Remove all python. Added bottemplate and added programmerbot

--- wikirace/wikirace.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, urllib.error, urllib.parse
-url = "https://en.wikipedia.org/wiki/Lou_Taylor_Pucci"
-response = urllib.request.urlopen(url)
-with open("wiki.html","w") as f:
-  f.write(str(response.read()))
